fc1f1ab1ae0a2bb6b7859a0d0d0ec220d71b512d
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-08-31 19:56:31 +0330

    feat(customer): register addresses and quotes modules

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { AddressesModule } from './addresses/addresses.module';
 import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './database/database.module';
+import { GeoModule } from './geo/geo.module';
 import { HealthController } from './health.controller';
+import { QuotesModule } from './quotes/quotes.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [DatabaseModule, RedisModule, AuthModule],
+  imports: [
+    DatabaseModule,
+    RedisModule,
+    AuthModule,
+    GeoModule,
+    AddressesModule,
+    QuotesModule,
+  ],
   controllers: [HealthController],
 })
 export class AppModule {}
