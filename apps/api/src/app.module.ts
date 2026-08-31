345e0206aed60c451aa1e7e14231552f6929c083
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-08-31 19:41:48 +0330

    feat(api): wire redis module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { HealthController } from './health.controller';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RedisModule],
   controllers: [HealthController],
 })
 export class AppModule {}
