c4d8529dadda529cb55706616714178c77e49f77
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-09-01 13:14:49 +0330

    Register realtime order event module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -8,12 +8,14 @@ import { GeoModule } from './geo/geo.module';
 import { HealthController } from './health.controller';
 import { OrdersModule } from './orders/orders.module';
 import { QuotesModule } from './quotes/quotes.module';
+import { RealtimeModule } from './realtime/realtime.module';
 import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     DatabaseModule,
     RedisModule,
+    RealtimeModule,
     AuthModule,
     GeoModule,
     AddressesModule,
