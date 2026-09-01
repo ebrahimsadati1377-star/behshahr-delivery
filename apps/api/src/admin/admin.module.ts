6fc026cee2fef1e984f2d1625d78574040f04618
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-09-01 13:22:12 +0330

    Register admin pricing service

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
+import { AdminPricingService } from './admin-pricing.service';
 import { AdminService } from './admin.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService],
+  providers: [AdminService, AdminPricingService],
 })
 export class AdminModule {}
