2d3c8a8fc61b92ed38678b9781c8ba04fb754e03
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-09-01 22:27:05 +0330

    Export OrdersService for integrations

--- apps/api/src/orders/orders.module.ts
+++ apps/api/src/orders/orders.module.ts
@@ -7,5 +7,6 @@ import { OrdersService } from './orders.service';
   imports: [QuotesModule],
   controllers: [OrdersController],
   providers: [OrdersService],
+  exports: [OrdersService],
 })
 export class OrdersModule {}
