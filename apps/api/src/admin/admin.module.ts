dff593693642f35eff00953dd9fb5f18e6c5d7c3
ebrahimsadati1377-star <ebrahimsadati1377@gmail.com>
2026-09-01 13:47:58 +0330

    Register admin service zone service

--- apps/api/src/admin/admin.module.ts
+++ apps/api/src/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminPricingService } from './admin-pricing.service';
+import { AdminServiceZoneService } from './admin-service-zone.service';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService, AdminPricingService],
+  providers: [AdminService, AdminPricingService, AdminServiceZoneService],
 })
 export class AdminModule {}
